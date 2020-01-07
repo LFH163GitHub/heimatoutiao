d9321252d3a6c4fbde740e0f71fc83f4b71a9fac
LFH <LFH_chunshou@163.com>
2020-01-07 21:04:13 +0800

    封装组件——输入框，并在登陆页面上使用（上）

---
+++ src/components/hminput.vue
@@ -0,0 +1,32 @@
+<template>
+  <input type="text" class="hminput" :class="{success:statu,error:!statu}"/>
+</template>
+
+<script>
+export default {
+    props:['rules','msg'],
+    data() {
+        return {
+            statu: true
+        }
+    },
+};
+</script>
+
+<style lang="less" scoped>
+.hminput {
+  width: 318/360 * 100vw;
+  height: 60px;
+  outline: none;
+  border: none;
+  border-bottom: 3px solid #ccc;
+  font-size: 20px;
+  line-height: 60px;
+}
+.success {
+  border-bottom-color: green;
+}
+.error {
+  border-bottom-color: red;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,11 +4,10 @@
       <div class="close"><span class="iconfont iconicon-test"></span></div>
       <div class="logo"><span class="iconfont iconnew"></span></div>
       <div class="inputs">
-        <input placeholder="请输入手机号" class="input" /><input
-          placeholder="密码"
-          class="input"
-          type="password"
-        />
+        <hminput
+          placeholder="请输入手机"
+        ></hminput>
+        <hminput placeholder="请输入密码"></hminput>
       </div>
       <p class="tips">
         没有账号？
@@ -20,10 +19,12 @@
 </template>
 
 <script>
-import hmbtn from '../components/hmbtn';
+import hmbtn from "../components/hmbtn";
+import hminput from "../components/hminput";
 export default {
-  components:{
-    hmbtn
+  components: {
+    hmbtn,
+    hminput
   }
 };
 </script>
