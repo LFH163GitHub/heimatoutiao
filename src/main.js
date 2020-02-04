483257891a191903af6696e54ee03b2ebb01a142
LFH <LFH_chunshou@163.com>
2020-02-04 10:52:35 +0800

    整个项目所用到的插件

--- src/main.js
+++ src/main.js
@@ -1,13 +1,40 @@
 import Vue from "vue";
 import App from "./App.vue";
-
-//使用vant框架的组件
-import { Toast } from "vant";
-Vue.use(Toast);
-
-
-Vue.config.productionTip = false;
 import router from "@/router/index.js";
+// vant框架里的Toast轻提示中间件
+import {
+  Toast,
+  Icon,
+  Uploader,
+  Dialog,
+  Field,
+  Picker,
+  Tab,
+  Tabs,
+  List,
+  PullRefresh,
+  SwipeCell,
+  Button,
+  Search
+} from "vant";
+// 引入全局样式
+import "@/styles/reset.css";
+//Toast轻提示
+Vue.use(Toast)
+  .use(Icon)
+  .use(Uploader)
+  .use(Dialog)
+  .use(Field)
+  .use(Picker)
+  .use(Tab)
+  .use(Tabs)
+  .use(List)
+  .use(PullRefresh)
+  .use(SwipeCell)
+  .use(Button)
+  .use(Search);
+Vue.config.productionTip = false;
+
 new Vue({
   router,
   render: h => h(App)
